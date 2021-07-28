3febb0a96056ea9945805726ee07bf2163cad087
JaimeSolisS <jaimesolissoto@me.com>
2021-07-27 18:06:13 -0600

    feat sort projects and buttons

--- src/Pages/Projects.js
+++ src/Pages/Projects.js
@@ -1,37 +1,41 @@
-import React, { useState } from 'react';
-import {MainLayout, InnerLayout} from '../styles/Layouts';
-import Title from '../Components/Title';
-import projects from '../data/projects';
-import Menu from '../Components/Menu';
-import Button from '../Components/Button';
+import React, { useState } from "react";
+import { MainLayout, InnerLayout } from "../styles/Layouts";
+import Title from "../Components/Title";
+import projects from "../data/projects";
+import Menu from "../Components/Menu";
+import Button from "../Components/Button";
 
-const allButtons = ['All', ...new Set(projects.map(item => item.category))]
+const sortedByName = projects.sort((a, b) =>
+  a.category.localeCompare(b.category)
+);
+const allButtons = [
+  "All",
+  ...new Set(sortedByName.map((item) => item.category)),
+];
 
 function Projects() {
- 
-    const sorted = projects.sort((a, b) => b.id - a.id);
-    const [menuItem, setMenuItems] = useState(sorted);
-    const [button, setButtons] = useState(allButtons);
+  const sorted = projects.sort((a, b) => b.id - a.id);
+  const [menuItem, setMenuItems] = useState(sorted);
+  const [button, setButtons] = useState(allButtons);
 
-    const filter = (button) => {
-
-        if(button === 'All'){
-            setMenuItems(sorted);
-            return;
-        }
-
-        const filteredData = sorted.filter(item => item.category === button);
-        setMenuItems(filteredData);
+  const filter = (button) => {
+    if (button === "All") {
+      setMenuItems(sorted);
+      return;
     }
-    return (
-        <MainLayout>
-            <Title title={'Projects'}/>
-            <InnerLayout>
-                <Button filter={filter} button={button} />
-                <Menu menuItem={menuItem} />
-            </InnerLayout>
-        </MainLayout>
-    )
+
+    const filteredData = sorted.filter((item) => item.category === button);
+    setMenuItems(filteredData);
+  };
+  return (
+    <MainLayout>
+      <Title title={"Projects"} />
+      <InnerLayout>
+        <Button filter={filter} button={button} />
+        <Menu menuItem={menuItem} />
+      </InnerLayout>
+    </MainLayout>
+  );
 }
 
-export default Projects
+export default Projects;
