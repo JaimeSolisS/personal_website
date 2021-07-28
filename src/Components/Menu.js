140c31153f86eb5572ff74094287aed6312c3137
JaimeSolisS <jaimesolissoto@me.com>
2021-07-27 18:04:27 -0600

    feat fix menu for projects

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -1,150 +1,168 @@
-import React from 'react'
-import styled from 'styled-components';
-import GitHub from '@material-ui/icons/GitHub';
-import { FaExternalLinkAlt } from 'react-icons/fa';
+import React from "react";
+import styled from "styled-components";
+import GitHub from "@material-ui/icons/GitHub";
+import { FaExternalLinkAlt } from "react-icons/fa";
 
-function Menu({menuItem}) {
- 
-    return (
-        <MenuItemStyled >
-            {
-                menuItem.map((item)=>{
-                    return <div className="grid-item" key={item.id}>
-                        <div className="portfolio-content">
-                            <div className="portfolio-image">
-                                <img src={item.image} alt=""/>
-                                <ul>
-                                {item.github != null &&
-                                        <li>
-                                            <a href={item.github}>
-                                            <GitHub />
-                                            </a>
-                                        </li>
-                                    }
-                                    {item.link != null &&
-                                        <li>
-                                            <a href={item.link}>
-                                            <FaExternalLinkAlt />
-                                            </a>
-                                        </li>
-                                    }
-                                </ul>
-                            </div>
-                            <h6>{item.title}</h6>
-                            <p>{item.text}</p>
-                        </div>
-                    </div>
-                })
-            }
-        </MenuItemStyled>
-    )
+function Menu({ menuItem }) {
+  return (
+    <MenuItemStyled>
+      {menuItem.map((item) => {
+        return (
+          <div className="grid-item" key={item.id}>
+            <div className="portfolio-content">
+              <div className="portfolio-image">
+                <img src={item.image} alt="" />
+                <ul>
+                  {item.github != null && (
+                    <li>
+                      <a
+                        href={item.github}
+                        rel="noopener noreferrer"
+                        target="_blank"
+                      >
+                        <GitHub />
+                      </a>
+                    </li>
+                  )}
+                  {item.link != null && (
+                    <li>
+                      <a
+                        href={item.link}
+                        rel="noopener noreferrer"
+                        target="_blank"
+                      >
+                        <FaExternalLinkAlt />
+                      </a>
+                    </li>
+                  )}
+                </ul>
+              </div>
+              <h6>{item.title}</h6>
+              <p>{item.text}</p>
+              <div className="tech-stack">
+                <p>{item.tech}</p>
+              </div>
+            </div>
+          </div>
+        );
+      })}
+    </MenuItemStyled>
+  );
 }
 
 const MenuItemStyled = styled.div`
-    display: grid;
-    grid-template-columns: repeat(3, 1fr);
-    grid-gap: 2rem;
-    @media screen and (max-width:920px){
-        grid-template-columns: repeat(2, 1fr);
-    }
-    @media screen and (max-width:670px){
-        grid-template-columns: repeat(1, 1fr);
-    }
-    .grid-item{
-        .portfolio-content{
-            display: block;
-            position: relative;
-            overflow: hidden;
-            h6{
-                font-size: 1.5rem;
-            }
-            img{
-                width: 100%;
-                height: 30vh;
-                object-fit: cover;
-            }
-            ul{
-                transform: translateY(-600px);
-                transition: all .4s ease-in-out;
-                position: absolute;
-                left: 50%;
-                top: 40%;
-                opacity: 0;
-                li{
-                        background-color: var(--border-color);
-                        display: flex;
-                        align-items: center;
-                        justify-content: center;
-                        padding: 1rem;
-                        border-radius: 50%;
-                        width: 3rem;
-                        height: 3rem;
-                        margin: 0 .5rem;
-                        transition: all .4s ease-in-out;
-                        &:hover{
-                            background-color: var(--primary-color);
-                        }
-                        a{
-                            display: flex;
-                            align-items: center;
-                            justify-content: center;
-                            transition: all .4s ease-in-out;
-                        }
-                    }
-            }
-            .portfolio-image{
-                &::before{
-                    content: "";
-                    position: absolute;
-                    left: 2%;
-                    top: 4%;
-                    height: 0;
-                    width: 0;
-                    transition: all .4s ease-in-out;
-                }
-            }
-            .portfolio-image:hover{
-                ul{
-                    transform: translateY(0);
-                    transform: translate(-50%, -50%);
-                    display: flex;
-                    align-items: center;
-                    justify-content: center;
-                    transition: all .4s ease-in-out;
-                    opacity: 1;
-                    li{
-                        transition: all .4s ease-in-out;
-                        &:hover{
-                            background-color: var(--primary-color);
-                        }
-                        a{
-                            display: flex;
-                            align-items: center;
-                            justify-content: center;
-                            transition: all .4s ease-in-out;
-                        }
-                    }
-                    li:hover{
-                        svg{
-                            color: var(--white-color);
-                        }
-                    }
-                    svg{
-                        font-size: 2rem;
-                    }
-                }
-                &::before{
-                    height: calc(100% - 32%) ;
-                    width: calc(100% - 4%);
-                    background-color: white;
-                    opacity: 0.9;
-                    transform-origin: left;
-                    
-                    transition: all .4s ease-in-out;
-                }
-            }
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 2rem;
+  @media screen and (max-width: 920px) {
+    grid-template-columns: repeat(2, 1fr);
+  }
+  @media screen and (max-width: 670px) {
+    grid-template-columns: repeat(1, 1fr);
+  }
+  .grid-item {
+    .portfolio-content {
+      display: block;
+      position: relative;
+      overflow: hidden;
+      h6 {
+        font-size: 1.5rem;
+      }
+      img {
+        width: 100%;
+        height: 30vh;
+        object-fit: cover;
+      }
+      ul {
+        transform: translateY(-600px);
+        transition: all 0.4s ease-in-out;
+        position: absolute;
+        left: 50%;
+        top: 40%;
+        opacity: 0;
+        li {
+          background-color: var(--border-color);
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          padding: 1rem;
+          border-radius: 50%;
+          width: 3rem;
+          height: 3rem;
+          margin: 0 0.5rem;
+          transition: all 0.4s ease-in-out;
+          &:hover {
+            background-color: var(--primary-color);
+          }
+          a {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            transition: all 0.4s ease-in-out;
+          }
         }
+      }
+      .tech-stack {
+        p {
+          font-size: 0.8rem;
+        }
+
+        word-spacing: 20px;
+        color: #8892b0;
+      }
+      .portfolio-image {
+        &::before {
+          content: "";
+          position: absolute;
+          left: 2%;
+          top: 4%;
+          height: 0;
+          width: 0;
+          transition: all 0.4s ease-in-out;
+        }
+      }
+      .portfolio-image:hover {
+        ul {
+          transform: translateY(0);
+          transform: translate(-50%, -50%);
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          transition: all 0.4s ease-in-out;
+          opacity: 1;
+          li {
+            transition: all 0.4s ease-in-out;
+            &:hover {
+              background-color: var(--primary-color);
+            }
+            a {
+              display: flex;
+              align-items: center;
+              justify-content: center;
+              transition: all 0.4s ease-in-out;
+            }
+          }
+          li:hover {
+            svg {
+              color: var(--white-color);
+            }
+          }
+          svg {
+            font-size: 2rem;
+          }
+        }
+        &::before {
+          height: calc(100% - 32%);
+          width: calc(100% - 4%);
+          background-color: white;
+          opacity: 0.9;
+          transform-origin: left;
+
+          transition: all 0.4s ease-in-out;
+        }
+      }
     }
+  }
 `;
 
 export default Menu;
